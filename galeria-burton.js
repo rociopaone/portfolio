242351e1cd99cf8f0d7280d70e77c2eba043019a
Rocio <ropaone28@gmail.com>
2024-06-14 03:04:29 -0300

    cositas nuevas

---
+++ galeria-burton.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const carouselTrack = document.querySelector('.carousel-track');
+    const images = document.querySelectorAll('.carousel-track img');
+    const imageWidth = images[0].clientWidth; // Ancho de cada imagen
+
+    let index = 0;
+
+    function nextSlide() {
+        index++;
+        if (index >= images.length) {
+            index = 0;
+        }
+        carouselTrack.style.transform = `translateX(-${index * imageWidth}px)`;
+    }
+
+    setInterval(nextSlide, 1500); // Cambia de imagen cada 3 segundos
+});
